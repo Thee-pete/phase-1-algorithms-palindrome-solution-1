d19ac2d979be2c7dedd268eb7006b4e32cafab89
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-19 00:24:02 +0300

    commit tree a74f8e7cbf6f7b3355a69236125e624d05330fe0

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
+  
+}
+function reverseString(word) {
+  // TODO: implement string reversing functionality
+  return word.split("").reverse().join("");
+}
+
+
+
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
